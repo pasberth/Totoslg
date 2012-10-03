732c61a3a3e318860485762ed112357e86a32376
pasberth <pasberth@gmail.com>
2012-10-03 22:31:36 +0900

    stageが追加されたら選べるようにした

--- lib/totoslg/window.rb
+++ lib/totoslg/window.rb
@@ -13,27 +13,20 @@ module Totoslg
     def initialize *args
       super
 
-      @start_window = create_sub(ConsoleWindow::Window, width, height, 0, 0)
+      @start_window = create_sub(ConsoleBoard::Board, width, height, 0, 0)
       @game_window = create_sub(GameWindow, width, height, 0, 0)
-
-      frames.before :main do
-        Totoslg::Stage.all.each_with_index do |stage, i|
-          @start_window.text[i] = "%20s: %s" % [stage.name, stage.details]
-        end
-      end
-
-      frames.after :main do
-        @start_window.text = ''
-      end
+      @start_window.table.row(0).width = width
+      @start_window.table.width = 1
+      @start_window.table.height = Totoslg::Stage.all.length
+      @start_window.table[0, 0] = '*'
 
       frames.on :main do
-        case getc
-        when 27.chr # ESC
-          unfocus!
-        when ' '    # Space
-          @game_window.focus!(:main, Totoslg::Stage.all.first.new)
-          unfocus!
+        Totoslg::Stage.all.each_with_index do |stage, i|
+          @start_window.table[0, i] = "%20s: %s" % [stage.name, stage.details]
         end
+        key, x, y = @start_window.focus!(:select, [' '])
+        @game_window.focus!(:main, Totoslg::Stage.all[y].new)
+        unfocus!
       end
 
       components << @start_window << @game_window
